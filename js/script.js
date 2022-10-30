44e4a1b0156ef7e9cfbabda6b48abe5cdaa313d7
faguzzi <aurelio_guzzi@yahoo.com.br>
2022-10-29 22:55:06 -0300

    desktop

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function mostrarMenu() {
+    // DOM JAVASCRIPT
+    let navbar = document.getElementById("menu-hamb");
+    navbar.classList.toggle("mostraMenu")
+    
+    console.log(navbar.classList);
+}
