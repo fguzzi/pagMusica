948ec999392d4f9b9958644864b579fa4c8fc2c8
faguzzi <aurelio_guzzi@yahoo.com.br>
2022-10-30 22:15:49 -0300

    laptop

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function mostrarMenu() {
     // DOM JAVASCRIPT
-    let navbar = document.getElementById("menu-hamb");
+    let navbar = document.getElementById("navMenu");
     navbar.classList.toggle("mostraMenu")
     
     console.log(navbar.classList);
